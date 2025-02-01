cfd9bc0b93c9b2fb59c267746187a8b0fac57394
Henry Chen <105070777+henqc@users.noreply.github.com>
2025-02-01 12:27:38 -0500

    Delete server directory

--- server/pinecone_server.py
+++
@@ -1,9 +0,0 @@
-from pinecone.grpc import PineconeGRPC as Pinecone
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-pinecone_key = os.getenv('pinecone_key')
-
-pc = Pinecone(api_key=pinecone_key)
